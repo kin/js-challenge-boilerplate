64d79ce9ae48a86d319559286ce1b13976e208a0
rbarnesokc <ryanbarnes.okc@gmail.com>
2025-07-04 14:20:33 -0500

    tests can run on chrome or brave

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,45 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  // Detect available browsers
+  const fs = require('fs');
+  const path = require('path');
+  
+  const bravePath = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser';
+  const chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  
+  let defaultBrowser = 'Chrome';
+  let customLaunchers = {
+    ChromeHeadlessCI: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox'],
+    },
+  };
+
+  // Check if Brave is available
+  if (fs.existsSync(bravePath)) {
+    defaultBrowser = 'Brave';
+    // Set CHROME_BIN environment variable for Brave
+    process.env.CHROME_BIN = bravePath;
+    customLaunchers.Brave = {
+      base: 'Chrome',
+      flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+      executablePath: bravePath
+    };
+    customLaunchers.BraveHeadless = {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
+      executablePath: bravePath
+    };
+  } else if (fs.existsSync(chromePath)) {
+    // Chrome is available
+    process.env.CHROME_BIN = chromePath;
+    customLaunchers.Chrome = {
+      base: 'Chrome',
+      flags: ['--no-sandbox', '--disable-setuid-sandbox']
+    };
+  }
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -16,13 +55,8 @@ module.exports = function (config) {
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
-    browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
-    },
+    browsers: [defaultBrowser],
+    customLaunchers: customLaunchers,
     singleRun: true,
   });
 };
