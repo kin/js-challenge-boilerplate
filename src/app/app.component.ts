9707ed20f276be0567e240b26a3310fd2315a0d8
Anson Hunnell <ansonhunnell@gmail.com>
2025-09-13 08:33:24 -0400

    feat(upload-csv): create initial UI w custom button and table styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { UploadCsvComponent } from './upload-csv/upload-csv.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    standalone: true
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [UploadCsvComponent], // 👈 keep this here
 })
 export class AppComponent {
   title = 'kin-ocr';
---
+++ src/app/upload-csv/upload-csv.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-csv',
+  standalone: true,
+  templateUrl: './upload-csv.component.html',
+  styleUrl: './upload-csv.component.scss',
+})
+export class UploadCsvComponent {}
