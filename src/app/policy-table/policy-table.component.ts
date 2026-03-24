f05e4be1698803e9e77adc82cb3876d80ecae274
Adam Beck <adambeck@Adams-Mac-mini.local>
2026-03-24 12:43:21 -0700

    User Story 2

--- src/app/policy-table/policy-table.component.ts
+++ src/app/policy-table/policy-table.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+interface Policy {
+  policyNumber: number;
+  isValid: boolean;
+}
 
 @Component({
   selector: 'app-policy-table',
@@ -8,6 +13,35 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
 })
-export class PolicyTableComponent {
+export class PolicyTableComponent implements OnChanges {
   @Input() policies: string[] = [];
+
+  processedPolicies: Policy[] = [];
+
+  ngOnChanges(): void {
+    this.processedPolicies = this.policies.map((p) => ({
+      policyNumber: parseInt(p, 10),
+      isValid: this.isValid(p),
+    }));
+  }
+
+  private isValid(policy: string): boolean {
+    const checksumArr = policy.split('').reverse();
+    const productArr = [];
+
+    for (let i = 0; i < checksumArr.length; i++) {
+      const char = checksumArr[i];
+      const num = parseInt(char, 10);
+      if (isNaN(num)) {
+        return false;
+      }
+
+      const product = num * (i + 1);
+      productArr.push(product);
+    }
+
+    const sum = productArr.reduce((acc, val) => acc + val, 0);
+
+    return sum % 11 === 0;
+  }
 }
