fe706617be60a11ea014caa2a5e9fe1c3a6ce6bf
Adam Beck <adambeck@Adams-Mac-mini.local>
2026-03-24 13:05:32 -0700

    Some basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { UploadFormComponent } from './upload-form/upload-form.component';
 import { PolicyTableComponent } from './policy-table/policy-table.component';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
+import { KinLogoComponent } from './kin-logo/kin-logo.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true,
-    imports: [UploadFormComponent, PolicyTableComponent, PageLayoutComponent]
+    imports: [UploadFormComponent, PolicyTableComponent, PageLayoutComponent, KinLogoComponent]
 })
 export class AppComponent {
   title = 'kin-ocr';
---
+++ src/app/kin-logo/kin-logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kin-logo',
+  templateUrl: './kin-logo.component.html',
+  styleUrl: './kin-logo.component.scss',
+  standalone: true,
+})
+export class KinLogoComponent {}
